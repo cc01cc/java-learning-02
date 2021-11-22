a3288a098f71bdcb71627047d8111009e747180a
cc01cc <cc01cc@foxmail.com>
2021-11-22 10:54:35 +0800

    调整 目标路径 将下划线改为 中划线，网址使用中划线

--- src-spring/eclipse-spring-workspace/spring-java-mybatis-java-01/src/main/java/com/cc01cc/spring/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.cc01cc.spring.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.cc01cc.spring.pojo.User;
-
-public interface LoginService {
-    public boolean checkIdentity(User user);
-}
--- src-spring/eclipse-spring-workspace/spring-java-mybatis-java-01/src/main/java/com/cc01cc/spring/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package com.cc01cc.spring.service;
-
-import com.cc01cc.spring.pojo.User;
-
-public interface RegisterService{
-    
-    public boolean register(User user);
-    
-}
