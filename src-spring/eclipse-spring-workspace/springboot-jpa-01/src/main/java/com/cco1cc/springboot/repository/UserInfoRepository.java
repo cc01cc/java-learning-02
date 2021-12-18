ff8094d0d510bd44c5eef71c94aead17c7df34ad
cc01cc <cc01cc@foxmail.com>
2021-12-18 19:06:43 +0800

    ADD: 添加持久层

---
+++ src-spring/eclipse-spring-workspace/springboot-jpa-01/src/main/java/com/cco1cc/springboot/repository/UserInfoRepository.java
@@ -0,0 +1,45 @@
+/**  
+ *   Copyright 2021 cc01cc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/**
+*   @Title: UserInfoRepository.java
+*   @Description: TODO
+*   @author cc01cc
+*   @date 2021-12-18 
+*/
+
+package com.cco1cc.springboot.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cco1cc.springboot.entity.UserInfo;
+
+/**
+ * @author       cc01cc
+ * @date         2021-12-18
+ * @Description: TODO
+ * 
+ */
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+
+    public UserInfo findbyUserAccount(String userAccount);
+
+    public boolean saveUserInfo(UserInfo userInfo);
+
+    public List<UserInfo> listUserInfobyTable(String tableName);
+}
